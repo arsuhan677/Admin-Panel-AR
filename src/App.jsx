a4696fdd250fa7c0ae7fe74a2b48d78dc0ea760d
Suhan <arsuhan677@gmail.com>
2025-11-18 01:48:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { Sidebar } from "lucide-react"
+import Sidebar from "./component/Layout/Sidebar"
+import Header from "./component/Layout/Header"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className='min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 transition-all duration-500'>
+      <div className='flex h-screen overflow-hidden'>
+      <Sidebar />
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Header />
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
